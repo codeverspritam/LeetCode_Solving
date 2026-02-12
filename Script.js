3e9c6fd62124d16c5a67120385bae15350bb1d8c
pritam <itspritamprasad@gmail.com>
2026-02-12 11:32:10 +0530

    Longest Common Prefix

--- Script.js
+++ Script.js
@@ -83,3 +83,24 @@ var isPalindrome = function (x) {
 
   return reverse == x;
 };
+
+/* Q4: Longest Common Prefix */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (!strs.length) return "";
+
+  for (let i = 0; i < strs[0].length; i++) {
+    const char = strs[0][i];
+
+    for (let j = 1; j < strs.length; j++) {
+      if (i >= strs[j].length || strs[j][i] !== char) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+
+  return strs[0];
+};
